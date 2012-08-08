b91709dbd1b2136c2a085ced358f3407c1d64fc0
Adrian Likins <alikins@redhat.com>
2012-08-08 10:26:09 -0400

    bundle a defaultdict workalike so we work on python2.4

--- pyqver2.py
+++ pyqver2.py
@@ -125,6 +125,51 @@ Identifiers = {
     "True":         (2, 2),
 }
 
+
+# from http://code.activestate.com/recipes/523034-emulate-collectionsdefaultdict/
+# since we'd like to run on python2.4 as well
+class defaultdict(dict):
+    def __init__(self, default_factory=None, *a, **kw):
+        if (default_factory is not None and
+            not hasattr(default_factory, '__call__')):
+            raise TypeError('first argument must be callable')
+        dict.__init__(self, *a, **kw)
+        self.default_factory = default_factory
+    def __getitem__(self, key):
+        try:
+            return dict.__getitem__(self, key)
+        except KeyError:
+            return self.__missing__(key)
+    def __missing__(self, key):
+        if self.default_factory is None:
+            raise KeyError(key)
+        self[key] = value = self.default_factory()
+        return value
+    def __reduce__(self):
+        if self.default_factory is None:
+            args = tuple()
+        else:
+            args = self.default_factory,
+        return type(self), args, None, None, self.items()
+    def copy(self):
+        return self.__copy__()
+    def __copy__(self):
+        return type(self)(self.default_factory, self)
+    def __deepcopy__(self, memo):
+        import copy
+        return type(self)(self.default_factory,
+                          copy.deepcopy(self.items()))
+    def __repr__(self):
+        return 'defaultdict(%s, %s)' % (self.default_factory,
+                                        dict.__repr__(self))
+
+
+def uniq(a):
+    if len(a) == 0:
+        return []
+    else:
+        return [a[0]] + uniq([x for x in a if x != a[0]])
+
 class NodeChecker(object):
     """
     A visitor, which traverses the syntax tree to find possible
