0f2a4d57875e6e40fa315e891f32c13cef07569e
Adrian Likins <alikins@redhat.com>
2014-06-19 10:55:59 -0400

    Add a simple setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+setup(name="pyqver",
+      version='1.3',
+      url="https://github.com/ghewgill/pyqver",
+      description="Identify the minimum Python version required for a given script.",
+      author="Greg Hewgill",
+      scripts=['pyqver2.py', 'pyqver3.py'])
